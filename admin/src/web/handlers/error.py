4861a8ffd50ca3ed930c728adcf605d4fb691f66
Conrado Federico Escobares <conradofescobares@gmail.com>
2025-09-11 23:24:45 -0300

    Añado la feature de manejo de errores

---
+++ admin/src/web/handlers/error.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from flask import render_template
+
+
+@dataclass
+class HTTPError:
+    code: int
+    message: str
+    description: str
+
+def not_found(error):
+    error = HTTPError(
+        code=404, 
+        message="Página no encontrada", 
+        description="La página que estás buscando no existe."
+    )
+
+    return render_template('error.html', error=error), 404
+
+def unauthorized(error):
+    error = HTTPError(
+        code=401, 
+        message="No autorizado", 
+        description="No tenés permisos para acceder a este recurso."
+    )
+
+    return render_template('error.html', error=error), 401
+
+def internal_server_error(error):
+    error = HTTPError(
+        code=500, 
+        message="Error interno del servidor", 
+        description="Ha ocurrido un error inesperado. Por favor, inténtalo de nuevo más tarde."
+    )
+
+    return render_template('error.html', error=error), 500
