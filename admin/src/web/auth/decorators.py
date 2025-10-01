b0fb0941ed3b6a39498e7bd965b5e4ac6f9ddaac
sofia spazzarini <sofiaspazzarini@gmail.com>
2025-09-30 22:00:52 -0300

    Cambio los permission_required() para que solo el admin pueda hacer operaciones CRUD del usuario

--- admin/src/web/auth/decorators.py
+++ admin/src/web/auth/decorators.py
@@ -3,12 +3,13 @@ from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
 from flask import jsonify
 from src.web.models import user as Usuario  
 
-def permission_required(permission):
+ADMIN_PERMISSIONS = ["user_new", "user_index", "user_show", "user_update", "user_destroy"]
+def permission_required(f):
     """
     Decorador para validar si el usuario tiene un permiso específico.
     """
-    def wrapper(fn):
-        @wraps(fn)
+    def wrapper(f):
+        @wraps(f)
         def decorator(*args, **kwargs):
             verify_jwt_in_request()
             user_id = get_jwt_identity()  # en el token guardamos el id del usuario
@@ -18,9 +19,9 @@ def permission_required(permission):
                 return jsonify({"error": "Usuario no encontrado"}), 404
 
             # Ejemplo simple: permisos basados en rol
-            if not user.permissions.contain(permission):
-                return jsonify({"error": "Acceso denegado, falta permiso"}), 403
+            if not all(user.permissions for perm in ADMIN_PERMISSIONS):
+                return jsonify({"error": "Acceso denegado, solo el admin puede acceder"}), 403
 
-            return fn(*args, **kwargs)
+            return f(*args, **kwargs)
         return decorator
     return wrapper
